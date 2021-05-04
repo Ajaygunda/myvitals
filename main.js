c4eff3bf02e49ff88ada47cc389cd1f070fd3b98
Ajaygunda <68199087+Ajaygunda@users.noreply.github.com>
2021-05-04 17:02:05 +0530

    Commit 4
    Added files

---
+++ main.js
@@ -0,0 +1,6 @@
+let x = document.getElementsByClassName("time")[0].innerText;
+let today = new Date();
+let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+
+document.getElementsByClassName("time")[0].innerText=today;
