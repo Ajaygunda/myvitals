6a370e81847e97f67a71e5f708bb7569a6803b21
Ajaygunda <68199087+Ajaygunda@users.noreply.github.com>
2021-05-04 17:33:32 +0530

    Commit 1

--- main.js
+++ main.js
@@ -3,4 +3,4 @@ let today = new Date();
 let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 
 
-document.getElementsByClassName("time")[0].innerText=today;
+document.getElementsByClassName("time")[0].innerText="4th May 5.33pm";
